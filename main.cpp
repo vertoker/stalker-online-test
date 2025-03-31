6ef2dff0521cbadc00ff753a3ffeeed5c97ca6bd
Kostya <vertogpro@gmail.com>
2025-03-31 17:29:33 +0500

    create first list and output... something

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
 
 int main()
 {
-    std::cout << "stalker online" << std::endl;
+    setlocale(LC_ALL, "Rus");
+
+    // wif for wchar_t
+    std::wifstream ifs("./list.txt", std::wifstream::in);
+
+    while (ifs)
+    {
+        wchar_t buffer;
+        ifs >> buffer;
+        std::cout << buffer; // wcout for wchar_t
+    }
+
+    std::cin.ignore(); // for console app
+
     return 0;
 }
